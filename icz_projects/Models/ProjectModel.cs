7cfafb26e2cb14cc4f74eb29e88937cf1b043c5b
kubalat <mail@tomaskubala.sk>
2019-05-22 18:14:04 +0200

    Add project

---
+++ icz_projects/Models/ProjectModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace icz_projects.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public string Customer { get; set; }
+    }
+}
