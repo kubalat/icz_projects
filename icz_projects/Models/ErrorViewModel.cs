ce77e60fd32ed7072ec89c60cf2a50831693959b
kubalat <mail@tomaskubala.sk>
2019-05-22 18:00:49 +0200

    Initial commit

---
+++ icz_projects/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace icz_projects.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
